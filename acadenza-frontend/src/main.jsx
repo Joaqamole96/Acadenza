ae3fb7da194a6c381dda89c5f8afe35e99e878cc
Joaqamole96 <144807741+Joaqamole96@users.noreply.github.com>
2025-09-17 17:38:46 +0800

    INIT: React Router; MINOR: Home.jsx

--- acadenza-frontend/src/main.jsx
+++ acadenza-frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import './styles/index.css'
+import Home from './pages/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<h1>Page not found.</h1>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ acadenza-frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+// import '../App.css'
+
+function Home() {
+    return (
+        <>
+            <h1>Home Page</h1>
+            <p>Welcome to Acadenza</p>
+        </>
+    )
+}
+
+export default Home
